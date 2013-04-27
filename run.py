dd5ac56e223cae1714d7b045fc8a93c348473b8c
Brad Van Vugt <bvanvugt@gmail.com>
2013-04-27 13:46:43 -0700

    Pass room to template.

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ bottle.TEMPLATE_PATH.append('static')
 
 @bottle.route('/:room')
 def index(room=None):
-    return bottle.template('index')
+    return bottle.template('index', room=room)
 
 bottle.run(host='localhost', port=8080)
