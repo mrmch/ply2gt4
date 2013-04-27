abba92eb5857f81251aeba53a4d0066db6e3c004
Matthew Harris <harrismc@gmail.com>
2013-04-27 15:10:40 -0700

    added jinja2

--- run.py
+++ run.py
@@ -9,11 +9,11 @@ appPath = dirname(__file__)
 @bottle.route('/:room')
 def index(room=None):
     bottle.TEMPLATES.clear()
-    return bottle.template('index', room=room)
+    return bottle.jinja2_template('index', room=room)
 
 @bottle.route('/static/js/:filename')
 def server_static(filename):
     return bottle.static_file(filename, root=join(appPath, 'static/js'))
 
-bottle.run(host='localhost', port=8080)
+bottle.run(host='localhost', port=8080, reloader=True)
 
