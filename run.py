c0bf2290be2134833ad0654cc24f38ac5f2c7a95
Nicholas Rempel <nbrempel@gmail.com>
2013-04-27 14:11:16 -0700

    added users route

--- run.py
+++ run.py
@@ -7,4 +7,10 @@ bottle.TEMPLATE_PATH.append('static')
 def index(room=None):
     return bottle.template('index', room=room)
 
+ @bottle.route('/users:user')
+def index(user=None):
+    return bottle.template('index', user=user)
+
+
+
 bottle.run(host='localhost', port=8080)
---
+++ static/js/firebasedata.js
@@ -0,0 +1,2 @@
+var roomsRef = new Firebase('https://ocf0s8prdzo.firebaseio-demo.com/rooms');
+
