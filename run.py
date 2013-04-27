76bbc62637a2af082decb8dab1ef090750127965
Brad Van Vugt <bvanvugt@gmail.com>
2013-04-27 14:04:23 -0700

    asd;nf;asdkf

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ bottle.TEMPLATE_PATH.append('static')
 
 @bottle.route('/:room')
 def index(room=None):
+    bottle.TEMPLATES.clear()
     return bottle.template('index', room=room)
 
 bottle.run(host='localhost', port=8080)
