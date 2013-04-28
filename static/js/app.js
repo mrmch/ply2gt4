9466674435b876146520051553ba0eb5f20dde57
Matthew Harris <harrismc@gmail.com>
2013-04-27 18:13:39 -0700

    loading shit!

--- static/js/app.js
+++ static/js/app.js
@@ -1,100 +1,78 @@
-//$(document).ready(function() {
+// youtube auth shit
+var google_api_key = 'AIzaSyDSw1FRDGu_3-I6lmoGjAGDUMM0dOgavZc';
 
-    // youtube auth shit
-    var google_api_key = 'AIzaSyDSw1FRDGu_3-I6lmoGjAGDUMM0dOgavZc';
+var userID = new Date().getTime();
 
-    var userID = new Date().getTime();
+//var roomRef = new Firebase('https://ply2gt5.firebaseio.com/rooms/' + PLY2GT4.room);
+//var usersRef = new Firebase('https://ply2gt5.firebaseio.com/rooms/' + PLY2GT4.room + '/users');
 
-    var roomRef = new Firebase('https://ply2gt5.firebaseio.com/rooms/' + PLY2GT4.room);
-    var usersRef = new Firebase('https://ply2gt5.firebaseio.com/rooms/' + PLY2GT4.room + '/users');
-
-    var userURL = 'https://ply2gt5.firebaseio.com/rooms/' + PLY2GT4.room + '/users/' + userID;
-    var playlistURL = 'https://ply2gt5.firebaseio.com/rooms/' + PLY2GT4.room + '/users/' + userID + '/playlist';
+var userURL = 'https://ply2gt4.firebaseio.com/rooms/' + PLY2GT4.room + '/users/' + userID;
+var playlistURL = 'https://ply2gt4.firebaseio.com/rooms/' + PLY2GT4.room + '/users/' + userID + '/playlist';
 
 
+angular.module('ply2gt4', ['firebase'], function ($provide) {
+    $provide.factory('playlistService', ['angularFire', function(angularFire) {
+        var playlistService = {};
 
-    /*
-    angular.module('phonecat', []).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-        when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
-        when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
-        otherwise({redirectTo: '/phones'});
-    }]);
-    */
+        playlistService.getPlaylist = function ($scope) {
+            var promise = angularFire(playlistURL, $scope, 'playlist', []);
+            promise.then(function () { 
+                $scope.addToPlaylist = function (clip) {
+                    $scope.playlist.push(clip);
+                };
 
-    angular.module('ply2gt4', ['firebase'], function ($provide) {
-        $provide.factory('playlistService', ['angularFire', function(angularFire) {
-            var playlistService = {};
-
-            playlistService.getPlaylist = function ($scope) {
-                var promise = angularFire(playlistURL, $scope, 'playlist', []);
-                promise.then(function () { 
-                    $scope.addToPlaylist = function (clip) {
-                        $scope.playlist.push(clip);
-                    };
-
-                    $scope.removeClip = function (clip) {
-                        $scope.playlist = [];
-                    };
-                })
-                return promise;
-            };
-
-            return playlistService;
-        }]);
-    })
-
-    .controller('PlaylistCtrl', ['$scope', 'playlistService', function ($scope, playlistService) {
-        playlistService.getPlaylist($scope);
-    }])
-
-    .controller('UserCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
-        var promise = angularFire(userURL, $scope, 'user', {});
-        promise.then(function () { 
-            $scope.user.matt = "asdf";
-            //var userRef = usersRef.child(userID);
-            //userRef.set({
-            //    $scope.user.set({
-            //    'username' : 'poopfeast'
-            //});
-            $scope.updateUsername = function () {
-                //$scope.user.child('username').set($scope.username);
-            };
-        });
-    }])
-
-    .controller('SearchCtrl', ['$scope', 'playlistService', function ($scope, playlistService) {
-        playlistService.getPlaylist($scope);
-        $scope.searchResults = [];
-
-        $scope.runSearch  = function () {
-            var q = $scope.searchText;
-            var request = gapi.client.youtube.search.list({
-                q: q,
-                maxResults: 10,
-                part: 'snippet'
-            });
-
-            request.execute(function(response) {
-                $scope.searchResults = [];
-                for (var i = 0; i < response.items.length; i++) {
-                    $scope.searchResults.push(response.items[i]);
-                }
-            });
+                $scope.removeClip = function (clip) {
+                    $scope.playlist = [];
+                };
+            })
+            return promise;
         };
 
+        return playlistService;
     }]);
+})
 
-    // Once the api loads call enable the search box.
-    function handleAPILoaded () {
-        $('#search-button').attr('disabled', false);
-    }
+.controller('PlaylistCtrl', ['$scope', 'playlistService', function ($scope, playlistService) {
+    playlistService.getPlaylist($scope);
+}])
 
-    // called once gapi client is loaded
-    function OnLoadCallback () {
-        gapi.client.setApiKey(google_api_key);
-        gapi.client.load('youtube', 'v3', handleAPILoaded);
-    }
+.controller('UserCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
+    var promise = angularFire(userURL, $scope, 'user', {});
+    pormiseB = promise.then(function () { 
+        $scope.user.username = new Date().getTime();
+    });
+}])
 
-//});
+.controller('SearchCtrl', ['$scope', 'playlistService', function ($scope, playlistService) {
+    playlistService.getPlaylist($scope);
+    $scope.searchResults = [];
+
+    $scope.runSearch  = function () {
+        var q = $scope.searchText;
+        var request = gapi.client.youtube.search.list({
+            q: q,
+            maxResults: 10,
+            part: 'snippet'
+        });
+
+        request.execute(function(response) {
+            $scope.searchResults = [];
+            for (var i = 0; i < response.items.length; i++) {
+                $scope.searchResults.push(response.items[i]);
+            }
+        });
+    };
+
+}]);
+
+// Once the api loads call enable the search box.
+function handleAPILoaded () {
+    $('#search-button').attr('disabled', false);
+}
+
+// called once gapi client is loaded
+function OnLoadCallback () {
+    gapi.client.setApiKey(google_api_key);
+    gapi.client.load('youtube', 'v3', handleAPILoaded);
+}
+
