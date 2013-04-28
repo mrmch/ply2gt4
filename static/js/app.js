b29789b30044e946a6ed9346981da16ebb47e89a
Brad Van Vugt <bvanvugt@gmail.com>
2013-04-27 19:49:16 -0700

    asdf

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,19 @@ function AppViewModel() {
 
     self.roomName = ko.observable(PLY2GT4.room);
     self.username = ko.observable('poopfeast');
+
+    ko.computed(function () {
+        var fbRoomURL = 'https://ply2gt5.firebaseio.com/' + self.roomName();
+        var fbRoomRef = new Firebase(fbRoomURL);
+
+        fbRoomRef.once('value', function (dataSnapshot) {
+            if (dataSnapshot.val() === null) {
+                fbRoomRef.child('users').push();
+            }
+        });
+
+        fbRoomRef.child('users').set({'one':'two'});
+    });
 };
 
 ko.applyBindings(new AppViewModel());
